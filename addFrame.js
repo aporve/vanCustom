19136c421810b32167fd510a7fd32c65d48dabb1
aporve <aporvep@gmail.com>
2021-06-09 22:04:18 +0530

    New Changes

--- addFrame.js
+++ addFrame.js
@@ -6,31 +6,31 @@
         `<div>
         <div style="position: absolute; top: 0; left: 0px; right: 0; bottom: 0;">
             <ul class="sc-eCApnc jvVggF" style="display: flex; flex-direction: column; justify-content: center; background-color: #fff; padding: 0; width: 3rem; list-style: none; height: calc(100% - 55px); justify-content: space-between;border: 1px solid #ccc">
-                <li class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 10px; cursor: pointer; display: flex;">
+                <li class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 7px; cursor: pointer; display: flex;">
                     <img src="https://cdn.yellowmessenger.com/LS9XYE9urVcj1605084428877.png" style="width: 35px; height: auto;" />
                 </li>
-                <li id="e1" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 0px; cursor: pointer; display: flex;">
+                <li id="e1" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 0px;  margin-left: -5; cursor: pointer; display: flex;">
                     <img src="https://cdn.yellowmessenger.com/GnxBWRVeIm6i1622800692538.png" style="width: 3.5rem; height: auto;" />
                 </li>
-                <li id="e2" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 0px; cursor: pointer; display: flex;">
+                <li id="e2" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 0px;  margin-left: -5; cursor: pointer; display: flex;">
                     <img src="https://cdn.yellowmessenger.com/4dnzmSDugrwW1622800597860.png" style="width: 3.5rem; height: auto;" />
                 </li>
-                <li id="e3" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 0px; cursor: pointer; display: flex;">
+                <li id="e3" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 0px;  margin-left: -5; cursor: pointer; display: flex;">
                     <img src="https://cdn.yellowmessenger.com/peOgkPdE8n0A1622800752931.png" style="width: 3.5rem; height: auto;" />
                 </li>
-                <li id="e4" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 0px; cursor: pointer; display: flex;">
+                <li id="e4" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 0px;  margin-left: -5; cursor: pointer; display: flex;">
                     <img src="https://cdn.yellowmessenger.com/FPloPRu3LmfA1622800812350.png" style="width: 3.5rem; height: auto;" />
                 </li>
-                <li id="e5" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 0px; cursor: pointer; display: flex;">
+                <li id="e5" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 0px;  margin-left: -5; cursor: pointer; display: flex;">
                     <img src="https://cdn.yellowmessenger.com/sULHcUPQH5RL1622800858305.png" style="width: 3.5rem; height: auto;" />
                 </li>
-              <li id="e6" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 0px; cursor: pointer; display: flex;">
+              <li id="e6" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 0px;  margin-left: -5; cursor: pointer; display: flex;">
                     <img src="https://cdn.yellowmessenger.com/VLla29GmwVpi1622800914321.png" style="width: 3.5rem; height: auto;" />
                 </li>
-              <li id="e7" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 0px; cursor: pointer; display: flex;">
+              <li id="e7" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 0px;  margin-left: -5; cursor: pointer; display: flex;">
                     <img src="https://cdn.yellowmessenger.com/o2BBDya1HiQT1622800956549.png" style="width: 3.5rem; height: auto;" />
               </li>
-              <li id="e8" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 0px; cursor: pointer; display: flex;">
+              <li id="e8" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 0px;  margin-left: -5; cursor: pointer; display: flex;">
                     <img src="https://cdn.yellowmessenger.com/g6Fdrr5NeUj01622800983140.png" style="width: 3.5rem; height: auto;" />
                 </li>
             </ul>
@@ -38,14 +38,14 @@
         <div style="position: fixed; top: 0; right: 0; width: calc(100% - 3rem - 2px); z-index: 1;">
             <ul class="sc-eCApnc jvVggF" style="display: flex; flex-direction: row; align-items: center; justify-content: flex-end; background-color: #fff; padding: 0; list-style: none; height: 3rem;border: 1px solid #ccc;border-left: 0">
                 <div style="justify-content: flex-start; width: 100%; padding: 0 0 0 20px;">
-                    <p id="faq">Vanguard</p>
+                    <p id="faq" style="font-weight: 700; font-size: x-large;color: black;">Vanguard</p>
                 </div>
                 <div style="display: flex;">
                    <!-- <ul> -->
+                        // <li id="e9" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer;">
+                        //     <img src="https://cdn.yellowmessenger.com/mv8YPnKAfFOm1623088433384.png" style="width: 1.2rem; height: auto;" />
+                        // </li>
                         <li id="e9" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer;">
-                            <img src="https://cdn.yellowmessenger.com/mv8YPnKAfFOm1623088433384.png" style="width: 1.2rem; height: auto;" />
-                        </li>
-                        <li id="e10" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer;">
                             <img src="https://cdn.yellowmessenger.com/DOZyHmvCcjee1623088538196.jpg" style="width: 1.2rem; height: auto;" />
                         </li>
                    <!-- </ul> -->
