ea5b015e878d4c9c66a3484153b5751b24d7a340
aporve <aporvep@gmail.com>
2021-06-16 11:56:17 +0530

    changes

--- addFrame.js
+++ addFrame.js
@@ -1,7 +1,7 @@
 (function addFrames () {
      try { 
         window.onresize=function(event){
-            var frameHeight=window.innerHeight-98-48;
+            var frameHeight=window.innerHeight-104-48;
             document.getElementById("chatBoxMainContainer").style.height=`${frameHeight}px`
         };
         document.getElementById('player').style.width= '0px'
